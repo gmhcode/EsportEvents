6e5f0a92d742c99e77deeeab06ba73f3802f9800
Greg Hughes <greghughes988@gmail.com>
2019-02-06 17:28:52 -0700

    Initial Commit

---
+++ EsportEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EsportEvents
+//
+//  Created by Greg Hughes on 2/6/19.
+//  Copyright © 2019 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
